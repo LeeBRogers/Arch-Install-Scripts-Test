fd823edeb402c7c79be081c587fc25e69d250e01
LeeBRogers <90856031+LeeBRogers@users.noreply.github.com>
2022-10-01 23:33:46 +0100

    Add files via upload

---
+++ base-uefi.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+hwclock --systohc
+sed -i '178s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP="uk" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+pacman -S networkmanager base-devel bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2  firewalld flatpak sof-firmware acpid
+
+pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+bootctl install
+
+#grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+# systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable libvirtd
+systemctl enable firewalld
+systemctl enable acpid
+
+useradd -m -G wheel -s /bin/bash lee
+echo ermanno:password | chpasswd
+
+echo "lee ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
+
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
+
+
+
