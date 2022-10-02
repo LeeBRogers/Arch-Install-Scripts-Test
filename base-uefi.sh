c639f6a4dd0d8aea7bb980d47af8ec44968473bf
LeeBRogers <90856031+LeeBRogers@users.noreply.github.com>
2022-10-02 20:33:38 +0100

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -5,25 +5,19 @@ hwclock --systohc
 sed -i '178s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "KEYMAP="uk" >> /etc/vconsole.conf
+echo "KEYMAP=uk" >> /etc/vconsole.conf
 echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
-
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
-
 pacman -S networkmanager base-devel bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2  firewalld flatpak sof-firmware acpid
-
 pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-
 bootctl install
-
 #grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
-
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
@@ -37,8 +31,7 @@ systemctl enable acpid
 useradd -m -G wheel -s /bin/bash lee
 echo lee:password | chpasswd
 
+useradd -m -G wheel -s /bin/bash lee
+echo lee:password | chpasswd
+
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-
-
-
-
