2d70593d9316f60c6a89eac43ec4603cb49b9962
LeeBRogers <90856031+LeeBRogers@users.noreply.github.com>
2022-10-02 21:47:50 +0100

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -13,7 +13,7 @@ echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
-pacman -S base-devel linux-headers networkmanager alsa-utils bluez bluez-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber firewalld virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft avahi cups  nss-mdns pacman-contrib reflector acpid acpid_call
+pacman -S base-devel linux-headers networkmanager alsa-utils bluez bluez-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber firewalld virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft avahi cups nss-mdns pacman-contrib reflector acpid acpid_call
 pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 bootctl --path=/boot install
